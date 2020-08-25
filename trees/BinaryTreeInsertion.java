44078720ee0a5d5174eebcfb81e3fc2040212d2f
willernsberger <willernsberger@gmail.com>
2020-08-24 23:26:17 -0700

    Commit.

--- trees/BinaryTreeInsertion.java
+++ trees/BinaryTreeInsertion.java
@@ -1,1 +1,75 @@
+import java.util.*;
+import java.io.*;
 
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+   
+  	public static void preOrder( Node root ) {
+      
+    	if( root == null)
+        	return;
+      
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+     
+    }
+
+	public static Node insert(Node root,int data) {
+        //evaluate root
+        //if root is null, create a Node, set data, return the node
+            //if data is greater, evaluate root.right
+                //if there is no root.right, crate node as root.right 
+                //if there is a root.right, call insert(root.right, data)
+            //if data is lesser, evaluate root.left
+                //if there is no root.left, create node as root.left
+                //if there is  a root.left, call insert(root.left, data)
+
+        if (root == null){
+            Node mNode = new Node(data);
+            return mNode;
+        }
+        if(data >= root.data){
+            if(root.right == null){
+                Node mNode = new Node(data);
+                root.right = mNode;
+            } 
+            else if (root.right != null){
+                insert(root.right, data);
+            }
+        }
+        if(data < root.data){
+            if(root.left == null){
+                Node mNode = new Node(data);
+                root.left = mNode;
+            } 
+            else if(root.left != null){
+                insert(root.left, data);
+            }
+        }
+    	return root;
+    }
+
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+      	preOrder(root);
+    }	
+}
