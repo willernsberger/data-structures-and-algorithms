484db340a68a670cdc3f88b7b2f92eddc69e7959
willernsberger <willernsberger@gmail.com>
2020-09-02 16:01:27 -0700

    Commit.

--- arrays/MinimumDistance.java
+++ arrays/MinimumDistance.java
@@ -6,6 +6,9 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+// HackerRank practice problem Minimum Absolute Difference in an Array
+// https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms
+
 public class MinimumDistance {
 
     // Complete the minimumAbsoluteDifference function below.
